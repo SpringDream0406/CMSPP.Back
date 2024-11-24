6ca3c4f925505ce790c308b7ec8185bd227c9680
SpringDream0406 <springdream0406@gmail.com>
2024-11-24 19:12:03 +0900

    auth guard 경로 수정해봄

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,10 @@ import { UserModule } from './apis/02.User/user.module';
 import { SppModule } from './apis/03.SPP/spp.module';
 import { databaseConfig } from './common/config/database.config';
 import { APP_GUARD } from '@nestjs/core';
-import { CustomAuthGuard } from './common/guard/customAuth.guard';
 import { CommonModule } from './common/common.module';
-// import { APP_INTERCEPTOR } from '@nestjs/core';
-// import { ResponseTimeInterceptor } from './common/interceptor/response-time.interceptor';
 import { validationSchema } from './common/config/validation.schema';
 import { ScheduleModule } from '@nestjs/schedule';
+import { CustomAuthGuard } from 'src/common/guard/customAuth.guard';
 
 @Module({
   imports: [
@@ -27,9 +25,6 @@ import { ScheduleModule } from '@nestjs/schedule';
     TypeOrmModule.forRootAsync(databaseConfig),
     ScheduleModule.forRoot(),
   ],
-  providers: [
-    // { provide: APP_INTERCEPTOR, useClass: ResponseTimeInterceptor },
-    { provide: APP_GUARD, useClass: CustomAuthGuard },
-  ],
+  providers: [{ provide: APP_GUARD, useClass: CustomAuthGuard }],
 })
 export class AppModule {}
