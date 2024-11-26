5105ec80831dab17a24746335246d2722ea43427
SpringDream0406 <springdream0406@gmail.com>
2024-11-26 12:11:47 +0900

    health check path add

--- src/apis/01.Auth/auth.controller.ts
+++ src/apis/01.Auth/auth.controller.ts
@@ -22,6 +22,11 @@ export class AuthController {
     private readonly configService: ConfigService,
   ) {}
 
+  @Get()
+  healthCheck(): string {
+    return 'OK';
+  }
+
   // 소셜 회원가입/로그인
   @Get('signup/:social')
   @Public()
