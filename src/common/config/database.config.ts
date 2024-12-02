6f9cdaae496a045eb1ebef98326b9e1e6180cd9f
SpringDream0406 <springdream0406@gmail.com>
2024-12-02 20:19:26 +0900

    ssl 설정, 스키마 설정

--- src/common/config/database.config.ts
+++ src/common/config/database.config.ts
@@ -1,9 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { envKeys } from './validation.schema';
+import * as fs from 'fs';
 
 export const databaseConfig = {
   useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+    const isProd = configService.get<string>(envKeys.env);
+
     return {
       type: configService.get<string>(envKeys.dbType) as 'postgres',
       host: configService.get<string>(envKeys.dbHost),
@@ -12,8 +15,13 @@ export const databaseConfig = {
       password: configService.get<string>(envKeys.dbPassword),
       database: configService.get<string>(envKeys.dbDatabase),
       autoLoadEntities: true, // 각각의 entity 일일이 입력 or 자동
-      synchronize: true, // 스키마 동기화
+      synchronize: isProd ? false : true, // 스키마 동기화
       logging: false, // 쿼리 실행 내역 터미널에 띄움
+      ssl: isProd
+        ? {
+            ca: fs.readFileSync('/etc/ssl/certs/ap-northeast-2-bundle.pem'),
+          }
+        : false,
     };
   },
   inject: [ConfigService],
